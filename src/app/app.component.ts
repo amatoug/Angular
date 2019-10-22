2d927b258f8c6226fe149afb7a1bd4736453cfe5
Ali Matoug <ali.matoug@softeam.fr>
2019-10-23 01:35:31 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pokemon-app',
+  template: `<h1>Bonjour, {{name}} !</h1>`,
+})
+export class AppComponent {
+  name = 'Angular';
+}
